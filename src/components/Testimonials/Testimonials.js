800e34c575908ffe5997382c421858d321d5f7e3
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2023-01-20 01:21:50 -0600

    migrated Testimonials section

---
+++ src/components/Testimonials/Testimonials.js
@@ -0,0 +1,166 @@
+import React from 'react';
+import './Testimonials.css';
+
+const Testimonials = () => {
+
+    return (
+        <section id="Testimonials2" class="s-testimonials target-section">
+
+            <div class="overlay"></div>
+
+            <div class="row testimonials-header">
+                <div class="col-full">
+                    <h1 class="h02">Testimonials</h1>
+                </div>
+            </div>
+
+            <div class="row testimonials">
+
+                <div class="col-full testimonials__slider">
+
+                    <div class="testimonials__slide">
+                        <a href="https://www.linkedin.com/in/david-mcallister-301250a8" target="_blank"
+                            rel="noreferrer noopener"><img src="IMGs/avatars/Dave_McAllister.jpg"
+                                                            alt="Testimonial Provider - Dave McAllister"
+                                                            class="testimonials__avatar" /></a>
+                        <p>Kassandra is extremely knowledgeable of Python and I got a lot out of her class.
+                            The material is very interactive and she helped me with any questions or issues that came up.</p>
+                        <div class="testimonials__author h06">
+                            <a class="str-testimonial-names" href="https://www.linkedin.com/in/david-mcallister-301250a8" target="_blank"
+                            rel="noreferrer noopener">Dave McAllister</a>
+                            <span>Senior Analyst - Global Pricing</span>
+                        </div>
+                    </div>
+
+                    <div class="testimonials__slide">
+                        <a href="https://www.linkedin.com/in/knickerbocker1/" target="_blank" rel="noreferrer noopener">
+                            <img src="IMGs/avatars/Jennifer_Knickerbocker.jpg"
+                                alt="Testimonial Provider - Jennifer Knickerbocker" class="testimonials__avatar" /></a>
+                        <p>I recall from our time together in the women's leadership course back in 2016, your tenacity and
+                            passion around teaching coding. You should walk away from your time at Cisco knowing that you
+                            have made an impact on so many people.<br />
+                            - Once a Cisconian, always a Cisconian.</p>
+                        <div class="testimonials__author h06">
+                            <a class="str-testimonial-names" href="https://www.linkedin.com/in/knickerbocker1/"
+                            target="_blank" rel="noreferrer noopener">Jennifer Knickerbocker</a>
+                            <span>Risk Management of Information, Systems, and Resources</span>
+                        </div>
+                    </div>
+
+                    <div class="testimonials__slide">
+                        <a href="https://www.linkedin.com/in/santanu-ganguly-70512a1/" target="_blank"
+                        rel="noreferrer noopener"><img src="IMGs/avatars/Santanu_Ganguly.jpg"
+                                                        alt="Testimonial Provider - Santanu Ganguly"
+                                                        class="testimonials__avatar" /></a><p>I have known you when you were
+                        training people on Python at Cisco, attended a lot of those coding hours and whereas I was your
+                        worst student and can’t code to save my life (not your fault), you were an amazing teacher and
+                        trainer.<br />Anyone willing to learn, upgrade or polish off Python skills will benefit amazingly from
+                        your sessions. Highly recommended!</p>
+                        <div class="testimonials__author h06">
+                            <a class="str-testimonial-names" href="https://www.linkedin.com/in/santanu-ganguly-70512a1/"
+                            target="_blank" rel="noreferrer noopener">Santanu Ganguly</a>
+                            <span>Systems Architect at Cisco | Author | Advisor | MSc. Observational Astrophysics,
+                                MSc. Mathematics, CEng, MBCS-CITP, Google Cloud Architect</span>
+                        </div>
+                    </div>
+
+                    <div class="testimonials__slide">
+                        <a href="https://www.linkedin.com/in/ariellesumits/" target="_blank" rel="noreferrer noopener">
+                            <img src="IMGs/avatars/Arielle_Sumits.jpg" alt="Testimonial Provider - Arielle Sumits"
+                                class="testimonials__avatar" /></a>
+                        <p>Kassandra you are a total go-getter! I have enjoyed partnering with you on Data Science for Good,
+                            and I am sure exciting adventures await you wherever you go.</p>
+                        <div class="testimonials__author h06">
+                            <a class="str-testimonial-names" href="https://www.linkedin.com/in/ariellesumits/"
+                            target="_blank" rel="noreferrer noopener">Arielle Sumits</a>
+                            <span>Data Scientist</span>
+                        </div>
+                    </div>
+
+                    <div class="testimonials__slide">
+                        <a href="https://www.linkedin.com/in/kirtee-yadav/" target="_blank" rel="noreferrer noopener">
+                            <img src="IMGs/avatars/Kirtee_Yadav.jpg" alt="Testimonial Provider - Kirtee Yadav"
+                                class="testimonials__avatar" /></a>
+                        <p>I really enjoyed working with you on DS4Good program, appreciate your leadership skills in
+                            building a team of volunteers and provide guidance on data science project!</p>
+                        <div class="testimonials__author h06">
+                            <a class="str-testimonial-names" href="https://www.linkedin.com/in/kirtee-yadav/"
+                            target="_blank" rel="noreferrer noopener">Kirtee Yadav</a>
+                            <span>Driving strategies to achieve key business outcomes</span>
+                        </div>
+                    </div>
+
+                    <div class="testimonials__slide">
+                        <a href="https://www.linkedin.com/in/larry-rios-b83a8866/" target="_blank"
+                        rel="noreferrer noopener"><img src="IMGs/avatars/Larry_Rios.jpg"
+                                                        alt="Testimonial Provider - Larry Rios"
+                                                        class="testimonials__avatar" /></a><p>Thank you so much for
+                        everything you have done to improve our ML and Data Science skills Kassandra. Also, thanks for
+                        helping new hires get connected to different groups in Cisco.</p>
+                        <div class="testimonials__author h06">
+                            <a class="str-testimonial-names" href="https://www.linkedin.com/in/larry-rios-b83a8866/"
+                            target="_blank" rel="noreferrer noopener">Larry Rios</a>
+                            <span>Software Developer at Cisco Systems</span>
+                        </div>
+                    </div>
+
+                    <div class="testimonials__slide">
+                        <a href="https://www.linkedin.com/in/david-spindola-15750274/" target="_blank"
+                        rel="noreferrer noopener"><img src="IMGs/avatars/David_Spindola.jpg"
+                                                        alt="Testimonial Provider - David Spindola"
+                                                        class="testimonials__avatar" /></a>
+                        <p>It was amazing to work with you Kassandra, your passion to share knowledge and make innovation
+                            happen is inspiring.</p>
+                        <div class="testimonials__author h06">
+                            <a class="str-testimonial-names" href="https://www.linkedin.com/in/david-spindola-15750274/"
+                            target="_blank" rel="noreferrer noopener">David Spindola</a>
+                            <span>Leader, Customer Delivery en Cisco (LATAM)</span>
+                        </div>
+                    </div>
+
+                    <div class="testimonials__slide">
+                        <a href="https://www.linkedin.com/in/hollyfitchstevens/" target="_blank"
+                        rel="noreferrer noopener"><img src="IMGs/avatars/holly-fitch-stevens.png"
+                                                        alt="Testimonial Provider - Holly Fitch-Stevens (Entrepreneur & Life Coach)"
+                                                        class="testimonials__avatar" /></a>
+                        <p>I am a total "TCBB"!!! That is a "Technically Challenged Baby Boomer"! Kassandra's patience and willingness made my sessions exciting and productive. She not only really knows her stuff on Facebook, she also helped me with my blog. Some very simple steps helped me get incredible engagement on my fan page, not to mention the "per click" savings when I learned basic Facebook advertising for very little money. You want to boost your business quickly and easily Kassandra is your gal!</p>
+                        <div class="testimonials__author h06">
+                            <a class="str-testimonial-names" href="https://www.linkedin.com/in/hollyfitchstevens/"
+                            target="_blank" rel="noreferrer noopener">Holly Fitch-Stevens</a>
+                            <span>Entrepreneur & Life Coach</span>
+                        </div>
+                    </div>
+
+                    <div class="testimonials__slide">
+                        <a href="https://www.linkedin.com/in/jlldfw/" target="_blank"
+                        rel="noreferrer noopener"><img src="IMGs/avatars/jaime-lewis.jpg"
+                                                        alt="Testimonial Provider - Jaime Lewis (Owner-Operator of Bizcardia)"
+                                                        class="testimonials__avatar" /></a>
+                        <p>Just by knowing Kassandra and watching her on social media, I have gained insight that has helped me to get better results in my own FB marketing. I expect that the ROI on her consultation service is a great value.</p>
+                        <div class="testimonials__author h06">
+                            <a class="str-testimonial-names" href="https://www.linkedin.com/in/jlldfw/"
+                            target="_blank" rel="noreferrer noopener">Jaime Lewis</a>
+                            <span>Owner-Operator of Bizcardia</span>
+                        </div>
+                    </div>
+
+                    <div class="testimonials__slide">
+                        <img src="IMGs/avatars/elisha-myatt.jpg"
+                            alt="Testimonial Provider - Elisha Myatt (Home Business Entrepreneur)"
+                            class="testimonials__avatar" />
+                        <p>Kassandra is an amazing Leader! Very down to earth, motivating, and love how she does her very best to help others ! Thank you for all your help I greatly appreciate it!!!</p>
+                        <div class="testimonials__author h06">
+                            <p class="str-testimonial-names">Elisha Myatt</p>
+                            <span>Home Business Entrepreneur</span>
+                        </div>
+                    </div>
+
+                </div> {/*<!-- end testimonials__slider -->*/}
+
+            </div> {/*<!-- end testimonials -->*/}
+
+        </section>
+    );
+}
+
+export default Testimonials;
