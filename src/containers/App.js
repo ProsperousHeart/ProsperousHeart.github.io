c09538536882b3aa40bec9b6beb8fc16156c724a
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2023-01-20 01:31:11 -0600

    integration of About, XP, Testimonials, Contact, Footer

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,18 @@
 // import React, { useState, useEffect } from 'react';
 import React from 'react';
-// import './App.css';
 import Nav from '../components/Nav/Nav';
 import Home from '../components/Home/Home';
+import About from '../components/About/About';
+import Exp from '../components/XP/XP';
+import Testimonials from '../components/Testimonials/Testimonials';
+import Contact from '../components/Contact/Contact';
+import Footer from '../components/Footer/Footer';
+
+import './00-base.css';
+import './00-sections.css';
+import './01-main.css';
+import './helpers.css';
+import './media.css';
 
 // require('dotenv').config();
 // const SMARTBRAIN_BE = process.env.REACT_APP_BE;
@@ -21,6 +31,11 @@ function App() { // original - functional component
       {/* <Navigation onRouteChg={this.onRouteChg} isSignedIn={isSignedIn} /> */}
       <Nav />
       <Home />
+      <About />
+      <Exp />
+      <Testimonials />
+      <Contact />
+      <Footer />
     </div>
   );
 
