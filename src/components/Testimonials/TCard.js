2a2d3e8b640a06cfe8ffe02a1d617837a2701e69
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2023-01-20 14:14:11 -0600

    update class to className in HTML for React

--- src/components/Testimonials/TCard.js
+++ src/components/Testimonials/TCard.js
@@ -2,28 +2,28 @@ import React from 'react';
 
 const TCard = ({ name, img, content, url, title }) => {
 
-    let html_name = <div class="str-testimonial-names">{name}</div>;
+    let html_name = <div className="str-testimonial-names">{name}</div>;
 
     let html_img = <img src={img}
                         alt={`Testimonial Provider - ${name}`}
-                        class="testimonials__avatar" />;
+                        className="testimonials__avatar" />;
     
     
     if (url.length !== 0) {
-        html_name=<a class="str-testimonial-names" href={url} target="_blank" rel="noreferrer noopener">{name}</a>;
+        html_name=<a className="str-testimonial-names" href={url} target="_blank" rel="noreferrer noopener">{name}</a>;
         html_img = <a href={url} target="_blank" rel="noreferrer noopener">
                         <img 
                             src={img} 
                             alt={`Testimonial Provider - ${name}`}
-                            class="testimonials__avatar" />
+                            className="testimonials__avatar" />
                     </a>;
     }
 
     return (
-        <div class="testimonials__slide">
+        <div className="testimonials__slide">
             { html_img }
             <p>{content}</p>
-            <div class="testimonials__author h06">
+            <div className="testimonials__author h06">
                 { html_name }
                 <span>{ title }</span>
             </div>
