5186dea59bab49b5e39a1bc368a3ac2b11deb424
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2023-01-31 20:36:33 -0600

    start of component migration for testimonials

---
+++ src/components/Testimonials/Accomplishment.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Accomplishment = ({ bulltPts }) => {
+
+    // let html_name = <div className="str-testimonial-names">{name}</div>;
+
+    // let html_img = <img src={img}
+    //                     alt={`Testimonial Provider - ${name}`}
+    //                     className="testimonials__avatar" />;
+    
+    
+    // if (url.length !== 0) {
+    //     html_name=<a className="str-testimonial-names" href={url} target="_blank" rel="noreferrer noopener">{name}</a>;
+    //     html_img = <a href={url} target="_blank" rel="noreferrer noopener">
+    //                     <img 
+    //                         src={img} 
+    //                         alt={`Testimonial Provider - ${name}`}
+    //                         className="testimonials__avatar" />
+    //                 </a>;
+    // }
+
+    // let bPnt = <li></li>
+
+    const bPts = data.map( (user, idx) => {
+        return (
+            <TCard
+                key={idx}
+                name={data[idx].name}
+                img={data[idx].img}
+                content={data[idx].content}
+                url={data[idx].url}
+                title={data[idx].title}
+                sliderSettings={settings}
+            />
+        );
+    });
+
+    return (
+        {bPts}
+    );
+}
+
+export default Accomplishment;
