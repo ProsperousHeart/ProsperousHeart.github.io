8d8f5e7a07a1e14a242d72d51f241b9614d69e33
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2023-05-09 00:17:07 -0500

    Addition of Projects section - still has work but wanted to get this into the repo

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import "./Projects.css";
+import ProjectList from './ProjectList';
+import { proj_data } from "./data";
+
+function ProjectSec({ data }) {
+    return (
+        <section id="Projects" className='s-projects'>
+            <div className='row projects-header'>
+                <div className="col-full">
+                    <h1 className="h02">Projects</h1>                    
+                </div>
+                <ProjectList data={data} />
+            </div>
+        </section>
+    );
+}
+
+// const Projects = () => {
+export default function Projects() {
+    // const proj_data = {
+    //     active_link: "",
+    //     project_name: "Project Name",
+    //     company: "Company Name",
+    //     desc: "Some desc here",
+    //     code: "https://github.com/ProsperousHeart",
+    //     youtube: ""
+    // }
+
+    return (
+        <section id="Projects">
+            <ProjectSec data={proj_data} />
+        </section>
+    );
+}
+
+// export default Projects;
---
+++ src/components/Projects/data.js
@@ -0,0 +1,72 @@
+export const proj_data = [
+    {
+        project_name: "Digital Resume",
+        subtitle: "Personal Project",
+        img: "https://i.imgur.com/ZTkt4I5.jpg",
+        img_alt: "fox",
+        company: "Prosperous Heart, LLC",
+        desc: "Project utilizing HTML/CSS & JavaScript then converted to React.",
+        code: "https://github.com/ProsperousHeart/prosperousHeart.github.io",
+        site: "https://resume.prosperousheart.com/",
+        youtube: "",
+        skills: [
+            "HTML/CSS",
+            "JavaScript",
+            "React",
+            "Bootstrap"
+        ]
+    },
+    {
+        project_name: "Python Bootcamp Training",
+        subtitle: "Personal Project",
+        img: "https://i.imgur.com/ZTkt4I5.jpg",
+        img_alt: "fox",
+        company: "Prosperous Heart, LLS",
+        desc: 'Jupyter notebooks teach python basics & provide opportunities to immediately implement learning.',
+        code: "https://github.com/ProsperousHeart/Basics-Boot-Camp/",
+        site: "https://github.com/ProsperousHeart/Basics-Boot-Camp/",
+        youtube: "",
+        skills: []
+    },
+    {
+        project_name: "Facial Recognition App (Back End)",
+        subtitle: "Bootcamp Project",
+        img: "https://i.imgur.com/ZTkt4I5.jpg",
+        img_alt: "fox",
+        company: "",
+        desc: "Leveraging Clarifai API, accurately detects faces in images. Has login to store count of times used per user.",
+        code: "https://github.com/ProsperousHeart/smart-brain-ZTM-render/tree/ZTM",
+        site: "https://smart-brain-ztm.onrender.com/",
+        youtube: "",
+        skills: []
+    },
+    {
+        project_name: "Facial Recognition App (Front End)",
+        subtitle: "Bootcamp Project",
+        img: "https://i.imgur.com/ZTkt4I5.jpg",
+        img_alt: "fox",
+        company: "",
+        desc: "Leveraging Clarifai API, accurately detects faces in images. Has login to store count of times used per user.",
+        code: "https://github.com/ProsperousHeart/FaceRecognitionApp-ZTM",
+        site: "https://resume.prosperousheart.com/FaceRecognitionApp-ZTM/",
+        youtube: "",
+        skills: []
+    },
+    {
+        project_name: "Robofriends",
+        subtitle: "Bootcamp Project",
+        img: "https://i.imgur.com/ZTkt4I5.jpg",
+        img_alt: "fox",
+        company: "Prosperous Heart, LLC",
+        desc: "React app that integerates 2 APIs to create searchable Robot cards.",
+        code: "https://github.com/ProsperousHeart/robofriends",
+        site: "https://resume.prosperousheart.com/robofriends/",
+        youtube: "",
+        skills: [
+            "HTML/CSS",
+            "JavaScript",
+            "React",
+            "Bootstrap"
+        ]
+    }
+]
