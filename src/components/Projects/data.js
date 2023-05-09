af71a1e3200f2a02e45647a2f344ab1068d969b6
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2023-05-09 14:59:26 -0500

    addition of 'active' if still working on project + prep for sorting via skills

--- src/components/Projects/data.js
+++ src/components/Projects/data.js
@@ -1,5 +1,18 @@
+export const skills = [
+    "python",
+    "HTML",
+    "CSS",
+    "JavaScript (JS)",
+    "React",
+    "Bootstrap",
+    "Jupyter Notebooks",
+    "Node.JS",
+    "postgreSQL"
+]
+
 export const proj_data = [
     {
+        active: true,
         project_name: "Digital Resume",
         subtitle: "Personal Project",
         img: "https://github.com/ProsperousHeart/ProsperousHeart.github.io/blob/master/public/IMGs/projs/ResumeSiteGIF-optimized.gif?raw=true",
@@ -9,14 +22,10 @@ export const proj_data = [
         code: "https://github.com/ProsperousHeart/prosperousHeart.github.io",
         site: "https://resume.prosperousheart.com/",
         youtube: "",
-        skills: [
-            "HTML/CSS",
-            "JavaScript",
-            "React",
-            "Bootstrap"
-        ]
+        skills: [1, 2, 3, 4, 5]
     },
     {
+        active: false,
         project_name: "Python Basics Bootcamp",
         subtitle: "Training Program",
         img: "https://github.com/ProsperousHeart/ProsperousHeart.github.io/blob/master/public/IMGs/projs/IMG-Basics-BC-optimized.png?raw=true",
@@ -26,21 +35,23 @@ export const proj_data = [
         code: "https://github.com/ProsperousHeart/Basics-Boot-Camp/",
         site: "https://github.com/ProsperousHeart/Basics-Boot-Camp/",
         youtube: "",
-        skills: []
+        skills: [0, 6]
     },
     {
+        active: false,
         project_name: "Facial Recognition App",
         subtitle: "Bootcamp Project",
         img: "https://github.com/ProsperousHeart/ProsperousHeart.github.io/blob/master/public/IMGs/projs/GIF-Backend-Server-optimized.gif?raw=true",
         img_alt: "GIF explaining project is a backend server",
         company: "Prosperous Heart, LLC",
-        desc: "Leveraging Clarifai API, accurately detects faces in images. Has login to store count of times used per user.",
+        desc: "Backend server portion for detectuing faces in images using Clarifai API. Has login to store count of times used per user.",
         code: "https://github.com/ProsperousHeart/smart-brain-ZTM-render/tree/ZTM",
         site: "https://smart-brain-ztm.onrender.com/",
         youtube: "",
-        skills: []
+        skills: [3, 7, 8]
     },
     {
+        active: false,
         project_name: "Facial Recognition App",
         subtitle: "Bootcamp Project",
         img: "https://github.com/ProsperousHeart/ProsperousHeart.github.io/blob/master/public/IMGs/projs/GIF-Face-Recognition.gif?raw=true",
@@ -50,9 +61,10 @@ export const proj_data = [
         code: "https://github.com/ProsperousHeart/FaceRecognitionApp-ZTM",
         site: "https://resume.prosperousheart.com/FaceRecognitionApp-ZTM/",
         youtube: "",
-        skills: []
+        skills: [1, 2, 3, 4, 5]
     },
     {
+        active: false,
         project_name: "Robofriends",
         subtitle: "Bootcamp Project",
         img: "https://github.com/ProsperousHeart/ProsperousHeart.github.io/blob/master/public/IMGs/projs/GIF-Robofriends-optimized.gif?raw=true",
@@ -62,11 +74,6 @@ export const proj_data = [
         code: "https://github.com/ProsperousHeart/robofriends",
         site: "https://resume.prosperousheart.com/robofriends/",
         youtube: "",
-        skills: [
-            "HTML/CSS",
-            "JavaScript",
-            "React",
-            "Bootstrap"
-        ]
+        skills: [1, 2, 3, 4, 5]
     }
 ]
