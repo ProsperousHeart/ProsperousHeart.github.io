07a50b4d97c5477ac107ad1866f8754212e4b849
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2023-05-09 14:42:08 -0500

    forced formatting

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 // function Project({data}) {
 //     return (
@@ -20,86 +20,88 @@ import React from 'react';
 //     )
 // }
 
-const ProjCard = ({ project_name, subtitle, img, img_alt, company, desc, 
-    code, site, youtube, skills, sliderSettings}) => {
+const ProjCard = ({
+  project_name,
+  subtitle,
+  img,
+  img_alt,
+  company,
+  desc,
+  code,
+  site,
+  youtube,
+  skills,
+  sliderSettings,
+}) => {
+  // let html_name = <div className="str-testimonial-names">{name}</div>;
 
-    // let html_name = <div className="str-testimonial-names">{name}</div>;
+  // let html_img = <img src={img}
+  //                     alt={`Testimonial Provider - ${name}`}
+  //                     className="testimonials__avatar" />;
 
-    // let html_img = <img src={img}
-    //                     alt={`Testimonial Provider - ${name}`}
-    //                     className="testimonials__avatar" />;
-    
-    
-    // if (url.length !== 0) {
+  // if (url.length !== 0) {
 
-    //     html_name=<a 
-    //                 className="str-testimonial-names" href={url}
-    //                 target="_blank" 
-    //                 rel="noreferrer noopener"
-    //                 >{name}</a>;
+  //     html_name=<a
+  //                 className="str-testimonial-names" href={url}
+  //                 target="_blank"
+  //                 rel="noreferrer noopener"
+  //                 >{name}</a>;
 
-    //     html_img = <a href={url} 
-    //                     target="_blank"
-    //                     rel="noreferrer noopener"
-    //                 >
-    //                 <img 
-    //                     src={img} 
-    //                     alt={`Testimonial Provider - ${name}`}
-    //                     className="testimonials__avatar"
-    //                 />
-    //                 </a>;
-    // }
+  //     html_img = <a href={url}
+  //                     target="_blank"
+  //                     rel="noreferrer noopener"
+  //                 >
+  //                 <img
+  //                     src={img}
+  //                     alt={`Testimonial Provider - ${name}`}
+  //                     className="testimonials__avatar"
+  //                 />
+  //                 </a>;
+  // }
 
-    // return (
-    //     <div className="col-full testimonials__slider">
-    //         <div className="testimonials__slide">
-    //             { html_img }
-    //             <p>{content}</p>
-    //             <div className="testimonials__author h06">
-    //                 { html_name }
-    //                 <span>{ title }</span>
-    //             </div>
-    //         </div>
-    //     </div>
-    // );
-    return (
-        <div className="card my-3 mx-auto">
-            <img
-                src={img}
-                className="card-img-top"
-                alt={img_alt}
-            />
-            {/* <div className='proj-content'> */}
-            <div className="card-body d-flex flex-column mb-3 pb-1">
-                <div className="card-info">
-                    <h5 className="card-title">{project_name}</h5>
-                    <h6 className="card-subtitle mb-2 text-muted">
-                        {subtitle}
-                    </h6>
-
-                    <p className="card-text py-2">{desc}</p>
-                </div>
-                <div className="card-btns d-flex justify-content-between mt-auto">
-                    <a
-                        href={site}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="btn mr-2"
-                    >
-                        <i className="fa fa-link"></i> Visit Site
-                    </a>
-                    <a
-                        href={code}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="btn "
-                    >
-                        <i className="fa fa-github"></i> Github
-                    </a>
-                </div>
-            </div>
+  // return (
+  //     <div className="col-full testimonials__slider">
+  //         <div className="testimonials__slide">
+  //             { html_img }
+  //             <p>{content}</p>
+  //             <div className="testimonials__author h06">
+  //                 { html_name }
+  //                 <span>{ title }</span>
+  //             </div>
+  //         </div>
+  //     </div>
+  // );
+  return (
+    <div className="card my-3 mx-auto">
+      <img src={img} className="card-img-top" alt={img_alt} />
+      {/* <div className='proj-content'> */}
+      <div className="card-body d-flex flex-column mb-3 pb-1">
+        <div className="card-info d-flex flex-column">
+          <h5 className="card-title">{project_name}</h5>
+          <h6 className="card-subtitle mb-2 text-muted">{subtitle}</h6>
+          <p className="card-text py-2">{desc}</p>
         </div>
-    );
-}
+        <div className="card-btns d-flex justify-content-between mt-auto">
+          <a
+            href={site}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn mr-2"
+          >
+            <i className="fa fa-link"></i> Visit Site
+          </a>
+          <a
+            href={code}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn "
+          >
+            <i className="fa fa-github"></i> Github
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ProjCard;
