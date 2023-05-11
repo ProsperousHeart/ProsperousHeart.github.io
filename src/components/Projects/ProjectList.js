3c916bfde60aead188d1032a8d58aedd527a8658
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2023-05-10 23:45:20 -0500

    update to Projects slider when mobile (turns off autoplay when down to 1 card)

--- src/components/Projects/ProjectList.js
+++ src/components/Projects/ProjectList.js
@@ -61,9 +61,9 @@ export default class ProjectList extends Component {
                 // },
                 {
                     breakpoint: 800,
-                    autoplaySpeed: 4000,
                     settings: {
-                        // autoplay: false,
+                        autoplay: false,
+                        // autoplaySpeed: 4000,
                         /* adaptiveHeight: true, /* https://www.freakyjolly.com/react-slick-carousel-with-custom-navigation-and-lazy-loaded-images-in-slider/ */
                         slidesToShow: 1,
                         slidesToScroll: 1
